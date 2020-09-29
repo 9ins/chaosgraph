661e2bcaf56a7c0b946c5c544474378005903a9a
chaos930 <chaos930@innoquartz.com>
2020-09-29 15:07:36 +0900

    - Modify README.MD

--- AWTExample2.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM SET JAVA_HOME=C:\j2sdk1.4.1_01
-
-REM SET path=%JAVA_HOME%\bin
-
-SET CLASS_PATH=./build/libs/chaosgraph-2.0.0.jar
-
-java -classpath %CLASS_PATH% org.chaostocosmos.chaosgraph.awt2d.GraphExample2
