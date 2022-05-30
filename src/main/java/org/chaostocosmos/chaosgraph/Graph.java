c4d1dfb21f7b6149bd2f90d7275ffa5e8e0e766d
9ins <tlsrndls2@>
2022-05-30 10:35:28 +0900

    20220530

--- src/main/java/org/chaostocosmos/chaosgraph/Graph.java
+++ src/main/java/org/chaostocosmos/chaosgraph/Graph.java
@@ -4,8 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
+import java.util.List;   
 
 /**
  * 
